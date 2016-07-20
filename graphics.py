3f7651808fb634e19d8c35aa3dab12a820e4a003
JNiel <jon.nielsen.swe@gmail.com>
2016-07-20 11:46:25 +0200

    Add files via upload
    First creation of graphics

---
+++ graphics.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 11:57:17 2016
+
+@author: jon.nielsen
+"""
+
+import numpy as np
+import scipy as sp
+import pandas
+import pylab
+import matplotlib.pyplot as plt
+
+#df = ABT_BRAND_TXN_ALLcsv
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+    
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure(1)
+plt.subplot(211)
+
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+
+
+
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
+
+fig = plt.figure(2)
+
+ax = fig.gca(projection='3d')
+X, Y, Z = axes3d.get_test_data(0.05)
+ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
+cset = ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
+
+ax.set_xlabel('X')
+ax.set_xlim(-40, 40)
+ax.set_ylabel('Y')
+ax.set_ylim(-40, 40)
+ax.set_zlabel('Z')
+ax.set_zlim(-100, 100)
+
+plt.show()
